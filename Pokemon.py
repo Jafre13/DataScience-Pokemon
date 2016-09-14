aa34f099e50d320182b21904121efff5b8165ac1
mrr <mads@deothan.dk>
2016-09-14 08:48:21 +0200

    change

--- Pokemon.py
+++ Pokemon.py
@@ -1,2 +1,10 @@
-print("I'm a Pokemon")
-print("So am I!")
+class Pokemon:
+
+    def __init__(self, name, life):
+        self.name = name
+        self.life = life
+
+p = Pokemon("Pickachu", 100)
+
+print(p.life)
+print(p.name)
