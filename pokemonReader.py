c27f719b19edb9e9fcda5d94bf3349f401918781
Jakob <jafre13@student.sdu.dk>
2016-09-14 09:43:45 +0200

    reads pokemon into csv file

--- pokemonReader.py
+++ pokemonReader.py
@@ -1,6 +1,30 @@
 import csv;
+import Pokemon
 
+list= [];
 file = open("pokemon.csv",newline="");
 reader = csv.reader(file)
+
+
 for row in reader:
-    print(row[0])
+    nr = row[0];
+    Name = row [1];
+    Type1 = row[2];
+    Type2 = row[3];
+    Total = row [4]
+    HP = row[5]
+    Attack = row[6]
+    Defence = row[7]
+    spAtack = row[8]
+    spDef = row[9]
+    Speed = row[10]
+    Generation = row[11]
+    Legendary = row[12]
+
+    pokemon = Pokemon.Pokemon(nr,Name,Type1,Type2,Total,HP,
+                      Attack,Defence,spAtack,spDef,
+                      Speed,Generation,Legendary);
+
+    list.append(pokemon)
+
+print(list[2].Name);
