3450468439ca51d084fb59a8ce27efcdab7aa29f
Jakob <jafre13@student.sdu.dk>
2016-09-14 10:00:06 +0200

    function readData now returns list of pokemoon

--- pokemonReader.py
+++ pokemonReader.py
@@ -2,29 +2,35 @@ import csv;
 import Pokemon
 
 list= [];
-file = open("pokemon.csv",newline="");
-reader = csv.reader(file)
+
+def readData():
+
+    file = open("pokemon.csv",newline="");
+    reader = csv.reader(file)
+
+    for row in reader:
+        nr = row[0];
+        Name = row [1];
+        Type1 = row[2];
+        Type2 = row[3];
+        Total = row [4]
+        HP = row[5]
+        Attack = row[6]
+        Defence = row[7]
+        spAtack = row[8]
+        spDef = row[9]
+        Speed = row[10]
+        Generation = row[11]
+        Legendary = row[12]
 
 
-for row in reader:
-    nr = row[0];
-    Name = row [1];
-    Type1 = row[2];
-    Type2 = row[3];
-    Total = row [4]
-    HP = row[5]
-    Attack = row[6]
-    Defence = row[7]
-    spAtack = row[8]
-    spDef = row[9]
-    Speed = row[10]
-    Generation = row[11]
-    Legendary = row[12]
-
-    pokemon = Pokemon.Pokemon(nr,Name,Type1,Type2,Total,HP,
+        pokemon = Pokemon.Pokemon(nr,Name,Type1,Type2,Total,HP,
                       Attack,Defence,spAtack,spDef,
                       Speed,Generation,Legendary);
+        list.append(pokemon)
 
-    list.append(pokemon)
 
-print(list[2].Name);
+    del list[0];
+
+    return list
+
